58501cc0940f7e9160c96e6ce3764c2e3e6123e2
code-project <dev@code-check.io>
2017-12-19 03:51:22 +0000

    Initial commit

---
+++ answer.md
@@ -0,0 +1,5 @@
+## Q1. 得意なプログラミング言語 Favorite Programming Language
+PHP
+
+## Q2. コードの説明 Code Explanation
+
---
+++ app/Main.php
@@ -0,0 +1,8 @@
+<?php namespace Codecheck;
+
+function run ($argc, $argv)
+{
+  foreach ($argv as $index=>$value) {
+    printf("Hello %s!\n", $value);
+  }
+}
